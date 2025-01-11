a055fe745e2e2958f1ee3f030c6ac0a7bd91bc55
kaxadeveloper <kaxa3developer@yahoo.com>
2025-01-11 10:45:27 +0400

    app

---
+++ app.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('button');
+
+button.addEventListener('click', function() {
+    let input = document.querySelector('input');
+    input.select();
+    document.execCommand('copy');
+})
